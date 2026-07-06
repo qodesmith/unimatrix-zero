edb0243ce1720fb1c6c0611b5e551f44fb58b782
Qodesmith <theqodesmith@gmail.com>
2026-07-06 06:51:07 -0400

    Install all shadcn components; wire up Tooltip, Toaster, ThemeProvider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,12 +1,20 @@
 import './assets/main.css'
 
+import {ThemeProvider} from 'next-themes'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 
 import App from './App'
+import {Toaster} from './components/ui/sonner'
+import {TooltipProvider} from './components/ui/tooltip'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <TooltipProvider>
+        <App />
+        <Toaster />
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>
 )
